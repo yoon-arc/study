cd734d75b5c92be53d96eff8f5b9381ecd81d98c
yoonlix <yoooon0718@naver.com>
2024-04-03 11:13:29 +0900

    js script first try

---
+++ test0081.js
@@ -0,0 +1,5 @@
+ var heading = document.querySelector('#heading');
+            heading.onclick = function(){
+            heading.style.color = "red";
+            //js는 html, css의 움직임을 보조해주는 것이 가장 중요
+        };
